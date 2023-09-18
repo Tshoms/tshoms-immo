e9e4b7ac3e49b1793f74807ed6310cd7d5880221
Tshoms <tshomsjordy@gmail.com>
2023-09-18 15:23:23 +0200

    F01/feature: Home.tsx > add some message

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,20 +2,35 @@ import React from 'react';
 import styled from "styled-components"
 import { useSearchParams } from 'react-router-dom';
 
-const Home: React.FC = () => {
+const Home = () => {
     // state --------------
     const [searchParams] = useSearchParams();
     const userName = searchParams.get("userName");
     console.log("value of userName:", userName);
 
     return (
-        <HomeStyled><p>Hello user {userName} !!!</p></HomeStyled>
+        <HomeStyled>
+            <h1>Hello : {userName} !!!</h1>
+            <h3>Don't panic Home page in process :) !!!</h3>
+        </HomeStyled>
     )
 
 }
 
 const HomeStyled = styled.div`
-  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1{
+      color: black;
+  }
+
+  h3{
+    font-family: 'Caveat', cursive;
+    font-size: 50px;
+  }
 `;
 
 export default Home
